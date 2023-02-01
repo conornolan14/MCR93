7d1e1624cde092a4e85b9c4ef2d567fec160f343
Stefan <mycourseresource@users.noreply.github.com>
2023-02-01 19:30:09 +0000

    Add files via upload

---
+++ 002-hello-world.py
@@ -0,0 +1,15 @@
+# This was last weeks exercise :-)
+print('Hello World')  # <= this prints one string
+
+# This is a print function to display Hello World as 3 strings
+print('Hello' + ' ' + 'World') # <= this prints a concatenated string
+
+print('Hello' * 3) # <= this prints three times the same string
+
+# This will print multiple arguments that are passed into print()
+print('Hello', 'World')
+
+print('My favourite number is', 7)
+
+# Solution:
+print('Temperature in Dublin:', 12, 'deg celsius')
