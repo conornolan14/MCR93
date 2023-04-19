c4af5e6359bc47f32adbef6fe0cf27bad66d7a87
Stefan <mycourseresource@users.noreply.github.com>
2023-04-19 19:04:26 +0100

    Update 008-a-address-data-dictionary.py

--- 008-a-address-data-dictionary.py
+++ 008-a-address-data-dictionary.py
@@ -1,13 +1,16 @@
 # LOGIN  / LOGOUT - Conor
-
 users = [
     {"username": "A Username", "password": "A Password"},
     {"username": "Conor", "password": "mcr93"}
     ]
 
+
+# Login Status
 logged_in = True    # A user is logged in and address data can be accessed
 logged_in = False   # A user is logged out and address data can not be accessed
 
+
+# Address Listing
 addrs = [
     {"f_name": "A first name",
      "l_name": "A last/family name",
@@ -21,7 +24,10 @@ addrs = [
      "number": "11-13",
      "eircode": "D01 F9C1",
      "phone": "01 - 666 1122"}
-
     ]
 
+
+# Looping example for the address list
+for i in range(len(addrs)):
+    print(addrs[i]['f_name'])
 
