a77e147f38d50296bec1fe2660122a48850d77fa
Stefan <mycourseresource@users.noreply.github.com>
2023-04-19 18:47:50 +0100

    Add files via upload

---
+++ 008-a-address-data-dictionary.py
@@ -0,0 +1,27 @@
+# LOGIN  / LOGOUT - Conor
+
+users = [
+    {"username": "A Username", "password": "A Password"},
+    {"username": "Conor", "password": "mcr93"}
+    ]
+
+logged_in = True    # A user is logged in and address data can be accessed
+logged_in = False   # A user is logged out and address data can not be accessed
+
+addrs = [
+    {"f_name": "A first name",
+     "l_name": "A last/family name",
+     "street": "A road/street name",
+     "number": "A house number",
+     "eircode": "Eircode/ZIP/Postal code",
+     "phone": "Phone Number"},
+    {"f_name": "Seamus",
+     "l_name": "Kavanagh",
+     "street": "O'Connel Street Upper",
+     "number": "11-13",
+     "eircode": "D01 F9C1",
+     "phone": "01 - 666 1122"}
+
+    ]
+
+
